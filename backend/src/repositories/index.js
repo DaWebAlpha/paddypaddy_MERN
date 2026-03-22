9ae3c6537dbfd3bb3a1397e0a19612830c0a79a9
DeWebAlpha <tribillio@gmail.com>
2026-03-22 10:36:14 +0000

    repositories

---
+++ backend/src/repositories/index.js
@@ -0,0 +1,13 @@
+import countryRepository from "./location/country.repository.js";
+import regionRepository from "./location/region.repository.js";
+
+
+export {
+    countryRepository,
+    regionRepository
+};
+
+export default {
+    countryRepository,
+    regionRepository
+};
