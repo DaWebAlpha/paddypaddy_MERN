777b6a5b4edb54e6195e88f896e421607e789ce2
DeWebAlpha <tribillio@gmail.com>
2026-03-26 21:26:29 +0000

    502 Bad Gateway error

---
+++ backend/src/errors/bad-gateway.error.js
@@ -0,0 +1,10 @@
+import AppError from "./app.error.js";
+
+class BadGatewayError extends AppError {
+    constructor(message = "Bad Gateway", details = null) {
+        super(message, 502, details);
+    }
+}
+
+export { BadGatewayError };
+export default BadGatewayError;
